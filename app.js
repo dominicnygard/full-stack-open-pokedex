ff4e553e83d7e95429456978c794091596cd761a
Dominic Nygård <dominicnygard0@gmail.com>
2025-12-28 21:03:46 +0200

    Fixing site so that deployment works again

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.listen(PORT + 1, () => {
+app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })
@@ -16,7 +16,5 @@ app.get('/version', (req, res) => {
 })
 
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw 'error...  '
   res.send('ok')
 })
